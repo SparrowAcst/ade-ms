5567b60e03431f78a585c83229d9e1585f4f2570
boldak <boldak.andrey@gmail.com>
2025-04-21 19:52:34 +0300

    arabia

--- src/spectrogram/spectrogram.config.js
+++ src/spectrogram/spectrogram.config.js
@@ -1,8 +1,9 @@
 
 const LABELING_COLLECTION = 'sparrow.clinic4'
-const LIMIT = 10
-const REFRESH_INTERVAL = 1 * 30 * 1000 // 1 min
+const LABELING_COLLECTION = 'sparrow.arabia-labels'
 
+const LIMIT = 60
+const REFRESH_INTERVAL = 1 * 30 * 1000 // 1 min
 
 module.exports = {
     LABELING_COLLECTION,
