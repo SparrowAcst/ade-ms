99470256dbd5bec4307176509ec2163e67c61bc7
boldak <boldak.andrey@gmail.com>
2025-04-22 15:28:59 +0300

    clinic4

--- src/spectrogram/spectrogram.config.js
+++ src/spectrogram/spectrogram.config.js
@@ -2,7 +2,7 @@
 // const LABELING_COLLECTION = 'sparrow.clinic4'
 const LABELING_COLLECTION = 'sparrow.clinic4'
 
-const LIMIT = 1
+const LIMIT = 60
 const REFRESH_INTERVAL = 1 * 30 * 1000 // 1 min
 
 module.exports = {
