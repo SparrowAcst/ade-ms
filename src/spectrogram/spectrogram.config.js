8b2a060cfb439ad43b0a5a6af89ca2bf1cb2ff9a
boldak <boldak.andrey@gmail.com>
2025-04-22 23:40:20 +0300

    add heap memory guard and promisify spectrogram build

--- src/spectrogram/spectrogram.config.js
+++ src/spectrogram/spectrogram.config.js
@@ -1,6 +1,10 @@
 
 // const LABELING_COLLECTION = 'sparrow.clinic4'
-const LABELING_COLLECTION = 'sparrow.hha'
+
+// const LABELING_COLLECTION = 'sparrow.vinil'
+// const LABELING_COLLECTION = 'sparrow.hha'
+
+const LABELING_COLLECTION = 'sparrow.innocent-reallife-labels'
 
 const LIMIT = 60
 const REFRESH_INTERVAL = 1 * 30 * 1000 // 1 min
