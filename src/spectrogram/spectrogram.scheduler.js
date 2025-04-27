f18dd5ef415b62922aac73383ecc387027f1033b
boldak <boldak.andrey@gmail.com>
2025-04-27 07:56:46 +0300

    set config for harvest 3 dataset

--- src/spectrogram/spectrogram.scheduler.js
+++ src/spectrogram/spectrogram.scheduler.js
@@ -1,6 +1,7 @@
 const uuid = require("uuid").v4
 const { extend } = require("lodash")
 const { AmqpManager, Middlewares } = require('@molfar/amqp-client');
+const uuid = require("uuid").v4
 
 const log = require("../utils//logger")(__filename)
 
